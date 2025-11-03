d077b0aff0cb9198b66b404578fdffffd133fe19
Melody Kimball <mforest@gsu.edu>
2025-11-03 09:34:41 -0500

    Updates to the course section page

--- src/app/pages/CoursesPage.tsx
+++ src/app/pages/CoursesPage.tsx
@@ -3,31 +3,37 @@ import Paper from "@mui/material/Paper";
 import PageLayout from "app/components/PageLayout";
 import Typography from "@mui/material/Typography";
 
-import { FilterSelect, SyllabusStatusSelect } from "app/pages/CoursesPage/selects";
+import { FilterSelect, SyllabusStatusSelect, CvStatusSelect } from "app/pages/CoursesPage/selects";
 import { CourseSectionsGrid } from "app/pages/CoursesPage/grids";
-import { UploadSyllabusModal } from "app/pages/CoursesPage/modals";
+import { UploadCvModal, UploadSyllabusModal } from "app/pages/CoursesPage/modals";
 
 export default function CoursesPage() {
   return (
     <PageLayout>
       <Paper elevation={2} sx={{ padding: "12px 16px" }}>
-        <Grid container spacing={{ xs: 12, md: 3 }} justifyContent="center">
+        <Grid container spacing={2} justifyContent="center">
           <Grid size={{ xs: 12 }}>
             <Typography component="h2" variant="h5" style={{ textAlign: "center" }}>
               Course Syllabus Portal
             </Typography>
           </Grid>
-          <Grid size={{ xs: 12, sm: 3 }}>
+          <Grid size={{ xs: 12, sm: 4 }}>
             <FilterSelect name="termCode" label="Term *" />
           </Grid>
-          <Grid size={{ xs: 12, sm: 3 }}>
+          <Grid size={{ xs: 12, sm: 4 }}>
             <FilterSelect name="collegeCode" label="College *" />
           </Grid>
-          <Grid size={{ xs: 12, sm: 3 }}>
+          <Grid size={{ xs: 12, sm: 4 }}>
             <FilterSelect name="departmentCode" label="Department *" />
           </Grid>
-          <Grid size={{ xs: 12, sm: 3 }}>
+          <Grid size={{ xs: 12, sm: 4 }} sx={{ display: { xs: "none", sm: "initial" } }}>
+            &nbsp;
+          </Grid>
+          <Grid size={{ xs: 12, sm: 4 }}>
             <SyllabusStatusSelect />
+          </Grid>
+          <Grid size={{ xs: 12, sm: 4 }}>
+            <CvStatusSelect />
           </Grid>
           <Grid size={12}>
             <CourseSectionsGrid />
@@ -35,6 +41,7 @@ export default function CoursesPage() {
         </Grid>
       </Paper>
       <UploadSyllabusModal />
+      <UploadCvModal />
     </PageLayout>
   );
 }
