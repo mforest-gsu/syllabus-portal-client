7b2e773b7bc3799d02f923efea8e646abe05d5d0
Melody Kimball <mforest@gsu.edu>
2025-11-04 09:33:32 -0500

    Build updates

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mforest-gsu/syllabus-portal-client
+
+Web frontend for Georgia State University's Syllabus Portal.
+
+## Deploying to production
+
+Double-check your `.env` files first, then run:
+
+```shell
+$ npm run build -- --base=/SyllabusPortal/ --minify
+```
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,30 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules/@mui/x-internal-exceljs-fork")) {
+            return "exceljs";
+          }
+          if (id.includes("node_modules/@mui/x-data-grid-pr")) {
+            return "mui-data-grid-pro";
+          }
+          if (id.includes("node_modules/@mui/x-data-grid")) {
+            return "mui-data-grid";
+          }
+          if (id.includes("node_modules/@mui") || id.includes("node_modules/@emotion")) {
+            return "mui";
+          }
+          if (id.includes("node_modules/")) {
+            return "vendor";
+          }
+          return "index";
+        },
+      },
+    },
+  },
   plugins: [
     tanstackRouter({
       target: "react",
